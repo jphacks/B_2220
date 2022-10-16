8d6290366250e641f7416bdbf02e07f82df30225
Yuri_Ishitoya <yuri1121isty1206@gmail.com>
2022-10-16 11:10:24 +0900

    インプット欄とSubmitボタンをつけた

--- src/views/RoomList.vue
+++ src/views/RoomList.vue
@@ -6,7 +6,7 @@
         shrink-on-scroll
     >
 
-      <v-toolbar-title>ルーム一覧</v-toolbar-title>
+      <v-toolbar-title>ルームの一覧</v-toolbar-title>
       <CreateRoom />
 
       <v-spacer></v-spacer>
@@ -17,42 +17,84 @@
     </v-app-bar>
 
     <v-main>
-      <v-container>
-        <v-row>
-          <v-col
-              v-for="room in rooms"
-              :key="room.id"
-              cols="4"
+      
+      <template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+        <p>{{ phoneNumber }}</p>
+        <p>1.発信する電話番号を入力してください</p>
+          <v-text-field
+            v-model="phoneNumber"
+            label="電話番号を入力してください"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+        <p>{{ name }}</p>
+        <p>2.あなたの名前を入力してください</p>
+          <v-text-field
+            v-model="name"
+            label="名前を入力してください"
+            required
+          ></v-text-field>
+        </v-col>
+        
+        <v-col
+          cols="12"
+          md="4"
+        >
+
+        <p>3.位置情報の取得を許可してください</p>
+        <div>
+        </div>
+        <template>
+        <div class="text-center">
+          <v-btn
+            class="ma-2"
+            :loading="loading"
+            :disabled="loading"
+            color="secondary"
+            @click="getLocation"
           >
-            <router-link :to="{ path: '/chat', query: { room_id: room.id }}">
-              <v-avatar color="grey lighten-2" size="128">
-                <img
-                  src="https://cdn.vuetifyjs.com/images/john.jpg"
-                  alt="John"
-                  v-if="!room.thumbnailUrl"
-                >
+            Accept Terms
+          </v-btn>
+          <p>{{ latitude }}</p>
+          <p>{{ longitude }}</p>
 
-                <img
-                  :src="room.thumbnailUrl"
-                  alt="John"
-                  v-if="room.thumbnailUrl"
-                >
-              </v-avatar>
-            </router-link>
+          </div>
+        </template>
 
-          </v-col>
-<!--          <v-col-->
-<!--              v-for="n in 24"-->
-<!--              :key="n"-->
-<!--              cols="4"-->
-<!--          >-->
-<!--            <router-link :to="{ path: '/chat', query: { user_id: n }}">-->
-<!--              <v-avatar color="grey lighten-2" size="128"></v-avatar>-->
-<!--            </router-link>-->
+        <p>4.あなたの名前を入力してください</p>
+        <template>
+          <div class="text-center">
+            <v-btn
+              class="ma-2"
+              :loading="loading"
+              :disabled="loading"
+              color="secondary"
+              @click="loader = 'loading'"
+            >
+              Accept Terms
+            </v-btn>
 
-<!--          </v-col>-->
-        </v-row>
-      </v-container>
+          </div>
+        </template>
+
+          
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
     </v-main>
   </v-app>
 </template>
@@ -68,12 +110,56 @@ export default {
     CreateRoom
   },
   data: () => ({
-    rooms: []
+    rooms: [],
+    name: '',
+    phoneNumber: '',
+    latitude: 0,
+     longitude: 0
   }),
   mounted() {
     this.getRooms()
   },
   methods: {
+    getLocation () {
+      console.log('getLocationCalled');
+     if (process.client) {
+       if (!navigator.geolocation) {
+         alert('現在地情報を取得できませんでした。お使いのブラウザでは現在地情報を利用できない可能性があります。エリアを入力してください。')
+         return
+       }
+
+       const options = {
+         enableHighAccuracy: false,
+         timeout: 5000,
+         maximumAge: 0
+       }
+
+       navigator.geolocation.getCurrentPosition(this.success, this.error, options)
+     }
+   },
+
+   success (position) {
+     this.latitude = position.coords.latitude
+     this.longitude = position.coords.longitude
+   },
+
+   error (error) {
+     switch (error.code) {
+       case 1: //PERMISSION_DENIED
+         alert('位置情報の利用が許可されていません')
+         break
+       case 2: //POSITION_UNAVAILABLE
+         alert('現在位置が取得できませんでした')
+         break
+       case 3: //TIMEOUT
+         alert('タイムアウトになりました')
+         break
+       default:
+         alert('現在位置が取得できませんでした')
+         break
+     }
+   },
+
     async getRooms() {
       this.rooms = []
       const roomRef = firebase.firestore().collection("rooms")
